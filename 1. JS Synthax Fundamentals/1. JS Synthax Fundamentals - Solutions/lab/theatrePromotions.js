1c15df45e44eef2ca4bad32e964b2f8827f5de8a
Dzhansu Zalim <djansuevil@gmail.com>
2024-02-24 11:49:00 +0200

    Create theatrePromotions.js

---
+++ 1. JS Synthax Fundamentals/1. JS Synthax Fundamentals - Solutions/lab/theatrePromotions.js
@@ -0,0 +1,39 @@
+function theatrePromotions(dayType, age) {
+
+    let price = 0;
+
+    if (dayType === 'Weekday') {
+        if (0 <= age && age <= 18) {
+            price = 12;
+        } else if (18 < age && age <= 64) {
+            price = 18;
+        } else if (64 < age && age <= 122) {
+            price = 12;
+        }
+    } else if (dayType === 'Weekend') {
+        if (0 <= age && age <= 18) {
+            price = 15;
+        } else if (18 < age && age <= 64) {
+            price = 20;
+        } else if (64 < age && age <= 122) {
+            price = 15;
+        }
+    } else {
+        if (0 <= age && age <= 18) {
+            price = 5;
+        } else if (18 < age && age <= 64) {
+            price = 12;
+        } else if (64 < age && age <= 122) {
+            price = 10;
+        }
+    }
+
+    if (price > 0) {
+        console.log(`${price}$`);
+    } else {
+        console.log('Error!');
+    }
+
+}
+
+theatrePromotions('Holiday', 15)
