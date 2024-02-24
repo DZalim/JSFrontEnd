12ceefa3d54bf9be50d6739fa8383aa7efa47917
Dzhansu Zalim <djansuevil@gmail.com>
2024-02-24 11:28:18 +0200

    Create monthPrinter.js

---
+++ 1. JS Synthax Fundamentals/1. JS Synthax Fundamentals - Solutions/lab/monthPrinter.js
@@ -0,0 +1,32 @@
+function monthPrinter(monthAsANumber) {
+    if (monthAsANumber === 1) {
+        console.log('January');
+    } else if (monthAsANumber === 2) {
+        console.log('February');
+    } else if (monthAsANumber === 3) {
+        console.log('March');
+    } else if (monthAsANumber === 4) {
+        console.log('April');
+    } else if (monthAsANumber === 5) {
+        console.log('May');
+    } else if (monthAsANumber === 6) {
+        console.log('June');
+    } else if (monthAsANumber === 7) {
+        console.log('July');
+    } else if (monthAsANumber === 8) {
+        console.log('August');
+    } else if (monthAsANumber === 9) {
+        console.log('September');
+    }else if (monthAsANumber === 10) {
+        console.log('October');
+    } else if (monthAsANumber === 11) {
+        console.log('November');
+    } else if (monthAsANumber === 12) {
+        console.log('December');
+    } else {
+        console.log('Error!');
+    }
+
+}
+
+monthPrinter(3)
