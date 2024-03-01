1061d97308a009dc896a10b96b43c0b960824abf
Dzhansu Zalim <djansuevil@gmail.com>
2024-03-01 09:21:07 +0200

    Create sumFirstAndLastArrayElements.js

---
+++ 2. JS Arrays & Strings/2. JS Arrays & Strings - Solutions/lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastArrayElements(array) {
+
+    let sumOfElements = array[0] + array[array.length - 1]
+
+    console.log(sumOfElements)
+}
+
+sumFirstAndLastArrayElements([20, 30, 40])
