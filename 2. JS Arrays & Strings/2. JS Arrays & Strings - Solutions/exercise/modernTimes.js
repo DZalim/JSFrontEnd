bcb1225c924da3f3166770d27a1297d0a341d97d
Dzhansu Zalim <djansuevil@gmail.com>
2024-03-02 18:53:41 +0200

    Create modernTimes.js

---
+++ 2. JS Arrays & Strings/2. JS Arrays & Strings - Solutions/exercise/modernTimes.js
@@ -0,0 +1,13 @@
+function modernTimes(text) {
+
+    let regex = /#[A-Za-z]+/gm
+    let matches = text.match(regex)
+
+    for (let word of matches) {
+        word = word.replace('#', '')
+        console.log(word)
+    }
+
+}
+
+modernTimes('Nowadays everyone uses # to tag a #special word in #socialMedia')
