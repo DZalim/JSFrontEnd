243008dffb91e6887b6e6bf62d5ea30afcbd391f
Dzhansu Zalim <djansuevil@gmail.com>
2024-03-08 10:44:32 +0200

    Create factorialDivision.js

---
+++ 3. Functions and Statements/3. Functions and Statements - Solutions/exercise/factorialDivision.js
@@ -0,0 +1,21 @@
+function factorialDivision(firstNum, secondNum) {
+
+  function calculateFactorial(number) {
+    let result = 1;
+
+    for (let i = 1; i <= number; i++) {
+      result *= i;
+    }
+
+    return result;
+  }
+
+  let firstResult = calculateFactorial(firstNum);
+  let secondResult = calculateFactorial(secondNum);
+  let division = firstResult / secondResult;
+
+  console.log(`${division.toFixed(2)}`);
+}
+
+factorialDivision(5, 2);
+factorialDivision(6, 2);
