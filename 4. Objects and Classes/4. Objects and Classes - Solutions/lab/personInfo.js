6e5db22c1312607c0a393b53234d35fe9292883e
Dzhansu Zalim <djansuevil@gmail.com>
2024-03-18 18:54:53 +0200

    create person.js

---
+++ 4. Objects and Classes/4. Objects and Classes - Solutions/lab/personInfo.js
@@ -0,0 +1,12 @@
+function personInfo(firstName, lastName, age) {
+  personObject = {
+    firstName,
+    lastName,
+    age,
+  };
+
+  return personObject
+}
+
+console.log(personInfo("Peter", "Pan", "20"));
+console.log(personInfo("George", "Smith", "18"));
